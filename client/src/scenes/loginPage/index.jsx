ed20a1323c47a903c521fdc207ea18ffa72dc54c
Saransh <saranshsingh199@gmail.com>
2023-09-05 21:12:03 +0530

    commit

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Connect
+          WeConnect
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Connecting Everyone
+        Welcome to WeConnect, Connecting minds and fostering growth!
         </Typography>
         <Form />
       </Box>
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
             },
           }}
         >
-          Connect
+          WeConnect
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
